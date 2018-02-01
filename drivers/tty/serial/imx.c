0f760e85a7d0e7b1a3d918056dc2e4e132b24b83
Daisuke Mizobuchi <mizo@atmark-techno.com>
2018-02-01 18:55:30 +0900

    serial: imx: get RS485 config from dt
    RS485がenableの場合のみ、RS485 configをdtから取得可能にした。
    
    RS485がdisableの場合は、当然アプリからRS485 configを設定する為、dtから
    取得する必要は無い。

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2116,6 +2116,7 @@ static int serial_imx_probe_dt(struct imx_port *sport,
 		struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
+	struct serial_rs485 *rs485conf = &sport->port.rs485;
 	int ret;
 
 	sport->devdata = of_device_get_match_data(&pdev->dev);
@@ -2139,6 +2140,27 @@ static int serial_imx_probe_dt(struct imx_port *sport,
 
 	if (of_get_property(np, "rts-gpios", NULL))
 		sport->have_rtsgpio = 1;
+
+	if (of_property_read_bool(np, "rs485-enabled-at-boot-time")) {
+		rs485conf->flags |= SER_RS485_ENABLED;
+
+		if (of_property_read_bool(np, "rs485-rx-during-tx"))
+			rs485conf->flags |= SER_RS485_RX_DURING_TX;
+
+		if (of_property_read_bool(np, "rs485-rts-on-send"))
+			rs485conf->flags |= SER_RS485_RTS_ON_SEND;
+
+		if (of_property_read_bool(np, "rs485-rts-after-send"))
+			rs485conf->flags |= SER_RS485_RTS_AFTER_SEND;
+
+		if (of_property_read_u32(np, "rs485-delay-rts-before-send",
+					 &rs485conf->delay_rts_before_send))
+			rs485conf->delay_rts_before_send = 0;
+
+		if (of_property_read_u32(np, "rs485-delay-rts-after-send",
+					 &rs485conf->delay_rts_after_send))
+			rs485conf->delay_rts_after_send = 0;
+	}
 
 	return 0;
 }
@@ -2207,8 +2229,10 @@ static int serial_imx_probe(struct platform_device *pdev)
 	sport->port.fifosize = 32;
 	sport->port.ops = &imx_pops;
 	sport->port.rs485_config = imx_rs485_config;
-	sport->port.rs485.flags =
-		SER_RS485_RTS_ON_SEND | SER_RS485_RX_DURING_TX;
+	if (!(sport->port.rs485.flags & SER_RS485_ENABLED)) {
+		sport->port.rs485.flags =
+			SER_RS485_RTS_ON_SEND | SER_RS485_RX_DURING_TX;
+	}
 	sport->port.flags = UPF_BOOT_AUTOCONF;
 	init_timer(&sport->timer);
 	sport->timer.function = imx_timeout;
@@ -2326,6 +2350,9 @@ static int serial_imx_probe(struct platform_device *pdev)
 	hrtimer_init(&sport->rs485_after_send, CLOCK_MONOTONIC,
 		     HRTIMER_MODE_REL);
 	sport->rs485_after_send.function = imx_rs485_after_send;
+
+	if (sport->port.rs485.flags & SER_RS485_ENABLED)
+		imx_rs485_config(&sport->port, &sport->port.rs485);
 
 	platform_set_drvdata(pdev, sport);
 
